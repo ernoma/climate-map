c45cde6a718f9de21f75eeb10e75b48db6912e10
xbexbex <petja.valkama@gmail.com>
2024-04-26 15:47:05 +0300

    Add useGetRoute

---
+++ src/common/hooks/routing/useGetRoute.tsx
@@ -0,0 +1,32 @@
+'use client'
+import { useUIStore } from '#/common/store'
+import { Params, RouteTree } from '#/common/types/routing'
+import { getRoute } from '#/common/utils/routing'
+
+/**
+ * The most accurate way for fetching a route within an applet.
+ */
+export const useGetRoute = (
+  route: RouteTree,
+  routeTree: RouteTree,
+  { routeParams = {}, queryParams = {} }: Params = {},
+  removeSteps = 0
+) => {
+  const isBaseDomainForApplet = useUIStore(
+    (state) => state.isBaseDomainForApplet
+  )
+
+  let removeStepsFromRoot = 0
+
+  if (routeTree.isAppletRoot && isBaseDomainForApplet) {
+    removeStepsFromRoot = 1
+  }
+
+  return getRoute(
+    route,
+    routeTree,
+    { routeParams, queryParams },
+    removeSteps,
+    removeStepsFromRoot
+  )
+}
