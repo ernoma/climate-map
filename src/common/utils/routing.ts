c391007688058c05607862057182d06b26709e0d
xbexbex <petja.valkama@gmail.com>
2024-04-26 15:43:58 +0300

    Update getRoute

--- src/common/utils/routing.ts
+++ src/common/utils/routing.ts
@@ -72,11 +72,43 @@ const findRouteObjects = (
   }
 }
 
+/**
+ * Creates a route from routeTree. Note that if used within an applet, the function might get called before the
+ * isBaseDomainForApplet is properly set, resulting in an incorrect path, depending on whether the applet
+ * is the root of the domain or not.
+ */
 export const getRoute = (
   route: RouteTree,
   routeTree: RouteTree,
   { routeParams = {}, queryParams = {} }: Params = {},
-  removeSteps = 0
+  removeSteps = 0,
+  removeStepsFromRoot = 0
+) => {
+  const isBaseDomainForApplet = useUIStore.getState().isBaseDomainForApplet
+
+  if (
+    removeStepsFromRoot === 0 &&
+    routeTree._conf.isAppletRoot &&
+    isBaseDomainForApplet
+  ) {
+    removeStepsFromRoot = 1
+  }
+
+  return getRouteNoStoreCheck(
+    route,
+    routeTree,
+    { routeParams, queryParams },
+    removeSteps,
+    removeStepsFromRoot
+  )
+}
+
+export const getRouteNoStoreCheck = (
+  route: RouteTree,
+  routeTree: RouteTree,
+  { routeParams = {}, queryParams = {} }: Params = {},
+  removeSteps = 0,
+  removeStepsFromRoot = 0
 ) => {
   let routeObjects = findRouteObjects(route, routeTree)
 
@@ -88,6 +120,13 @@ export const getRoute = (
 
   if (routeObjects.length === 0) {
     throw new Error('Route not found: ' + route + ' in ' + routeTree)
+  }
+
+  if (removeStepsFromRoot > 0) {
+  }
+
+  for (let i = 0; i < removeStepsFromRoot; i++) {
+    routeObjects.shift()
   }
 
   let path = ''
