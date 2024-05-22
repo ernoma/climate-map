e84c75c42b7b0082848b0b4ab36b05b8d8cf12f9
xbexbex <petja.valkama@gmail.com>
2024-05-22 19:01:55 +0300

    Comment out unused openlayers stuff

--- src/common/utils/map.ts
+++ src/common/utils/map.ts
@@ -1,8 +1,8 @@
 import { Expression, GeoJSONSource, MapboxGeoJSONFeature } from 'mapbox-gl'
-import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style'
-import Layer from 'ol/layer/Layer'
-import WebGLVectorLayerRenderer from 'ol/renderer/webgl/VectorLayer'
-import { asArray } from 'ol/color'
+// import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style'
+// import Layer from 'ol/layer/Layer'
+// import WebGLVectorLayerRenderer from 'ol/renderer/webgl/VectorLayer'
+// import { asArray } from 'ol/color'
 // import { packColor } from 'ol/renderer/webgl/shaders'
 import { Feature, Geometry, Position } from 'geojson'
 import { Map } from 'mapbox-gl'
@@ -23,68 +23,68 @@ import { clone, uniqBy } from 'lodash-es'
 
 export const fillOpacity = 0.65
 
-const defaultVectorStyles: any = {
-  LineString: new Style({
-    stroke: new Stroke({
-      color: 'green',
-      width: 1,
-    }),
-  }),
-  MultiLineString: new Style({
-    stroke: new Stroke({
-      color: 'green',
-      width: 1,
-    }),
-  }),
-  MultiPolygon: new Style({
-    stroke: new Stroke({
-      color: 'yellow',
-      width: 1,
-    }),
-    fill: new Fill({
-      color: 'rgba(255, 255, 0, 0.1)',
-    }),
-  }),
-  Polygon: new Style({
-    stroke: new Stroke({
-      color: 'blue',
-      lineDash: [4],
-      width: 3,
-    }),
-    fill: new Fill({
-      color: 'rgba(0, 0, 255, 0.1)',
-    }),
-  }),
-  GeometryCollection: new Style({
-    stroke: new Stroke({
-      color: 'magenta',
-      width: 2,
-    }),
-    fill: new Fill({
-      color: 'magenta',
-    }),
-    image: new CircleStyle({
-      radius: 10,
-      fill: undefined,
-      stroke: new Stroke({
-        color: 'magenta',
-      }),
-    }),
-  }),
-  Circle: new Style({
-    stroke: new Stroke({
-      color: 'red',
-      width: 2,
-    }),
-    fill: new Fill({
-      color: 'rgba(255,0,0,0.2)',
-    }),
-  }),
-}
+// const defaultVectorStyles: any = {
+//   LineString: new Style({
+//     stroke: new Stroke({
+//       color: 'green',
+//       width: 1,
+//     }),
+//   }),
+//   MultiLineString: new Style({
+//     stroke: new Stroke({
+//       color: 'green',
+//       width: 1,
+//     }),
+//   }),
+//   MultiPolygon: new Style({
+//     stroke: new Stroke({
+//       color: 'yellow',
+//       width: 1,
+//     }),
+//     fill: new Fill({
+//       color: 'rgba(255, 255, 0, 0.1)',
+//     }),
+//   }),
+//   Polygon: new Style({
+//     stroke: new Stroke({
+//       color: 'blue',
+//       lineDash: [4],
+//       width: 3,
+//     }),
+//     fill: new Fill({
+//       color: 'rgba(0, 0, 255, 0.1)',
+//     }),
+//   }),
+//   GeometryCollection: new Style({
+//     stroke: new Stroke({
+//       color: 'magenta',
+//       width: 2,
+//     }),
+//     fill: new Fill({
+//       color: 'magenta',
+//     }),
+//     image: new CircleStyle({
+//       radius: 10,
+//       fill: undefined,
+//       stroke: new Stroke({
+//         color: 'magenta',
+//       }),
+//     }),
+//   }),
+//   Circle: new Style({
+//     stroke: new Stroke({
+//       color: 'red',
+//       width: 2,
+//     }),
+//     fill: new Fill({
+//       color: 'rgba(255,0,0,0.2)',
+//     }),
+//   }),
+// }
 
-export const defaultVectorStyleFunction = (feature: any) => {
-  return defaultVectorStyles[feature.getGeometry().getType()]
-}
+// export const defaultVectorStyleFunction = (feature: any) => {
+//   return defaultVectorStyles[feature.getGeometry().getType()]
+// }
 
 // export class WebGLLayer extends Layer {
 //   createRenderer = (): any => {
