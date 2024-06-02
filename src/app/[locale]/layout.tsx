39b98f1c626dfd72540b5dc33b8bb0ce04549733
xbexbex <petja.valkama@gmail.com>
2024-06-02 19:43:25 +0300

    Add routing logic to check validity of locale

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import { redirect } from 'next/navigation'
 
-import { notFound } from 'next/navigation'
 import LayoutClient from './layoutClient'
-import { ALL_NS_LANGS, getStaticData } from '#/common/tolgee/shared'
+import {
+  getStaticData,
+  DEFAULT_LOCALE,
+  getLocaleObj,
+  getLocalesForNs,
+  ALL_NS_LANGS,
+} from '#/common/tolgee/shared'
 import { TolgeeNextProvider } from '#/common/tolgee/client'
+import { pathnames } from '#/components/Navigation'
 
 type Props = {
   children: React.ReactNode
   params: { locale: string }
+  resolvedUrl: string
 }
 
 const Layout = async ({
@@ -15,23 +23,60 @@ const Layout = async ({
   // This will be populated with nested layouts or pages
   children,
   params: { locale },
+  resolvedUrl,
 }: Props) => {
   // const locales = await getStaticData([locale])
-  const getLocaleObj = (locale: string) => {
-    const locales: Record<string, { langs: string[] }> = {}
-    for (const ns of Object.keys(ALL_NS_LANGS)) {
-      // @ts-ignore
-      if (ALL_NS_LANGS[ns].langs.includes(locale)) {
-        locales[ns] = { langs: [locale] }
+
+  let localeObj = getLocaleObj(locale)
+  const localeExists = Object.keys(localeObj).length > 0
+  const splitUrl = resolvedUrl.split('/').filter((path) => path !== '')
+
+  if (!localeExists || splitUrl.length > 1) {
+    let matchedPath = null
+
+    // the index of the part of the url that is used for pattern matching
+    // i.e., the part after locale. Or if locale is not found, the first part
+    let urlIndex = 0
+    if (localeExists && splitUrl.length > 1) {
+      urlIndex = 1
+    }
+
+    matchedPath = Object.values(pathnames).find((pathname) => {
+      const splitPathname = pathname.split('/').filter((path) => path !== '')
+      if (splitPathname.length > 0) {
+        return (
+          splitPathname[0].toLowerCase() === splitUrl[urlIndex].toLowerCase()
+        )
+      }
+    })
+
+    // if the path is legit
+    if (matchedPath) {
+      if (Object.keys(ALL_NS_LANGS).includes(matchedPath)) {
+        let appletLocale = locale
+        if (!localeExists) {
+          appletLocale = DEFAULT_LOCALE
+        }
+        const appletPath = matchedPath as keyof typeof ALL_NS_LANGS
+        const localesForNs = getLocalesForNs(appletPath)
+        if (localesForNs.includes(locale)) {
+          if (!localeExists) {
+            // redirect to the global default locale
+            redirect(`/${appletLocale}/${resolvedUrl}`)
+          }
+        } else {
+          appletLocale = localesForNs[0]
+
+          // redirect to default (first) locale for the applet
+          redirect(`/${appletLocale}/${resolvedUrl}`)
+        }
       }
     }
-    return locales
-  }
 
-  const localeObj = getLocaleObj(locale)
-
-  if (Object.keys(localeObj).length === 0) {
-    notFound()
+    if (!localeExists) {
+      // redirect to the global default locale
+      redirect(`/${DEFAULT_LOCALE}/${resolvedUrl}`)
+    }
   }
 
   const locales = await getStaticData(getLocaleObj(locale))
