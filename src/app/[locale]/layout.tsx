2081c206c52cb215bfc15e4e17a05b64f444391a
xbexbex <petja.valkama@gmail.com>
2024-06-04 21:20:44 +0300

    Move locale routing from layout to middleware

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,16 +1,8 @@
 import React from 'react'
-import { redirect } from 'next/navigation'
 
 import LayoutClient from './layoutClient'
-import {
-  getStaticData,
-  DEFAULT_LOCALE,
-  getLocaleObj,
-  getLocalesForNs,
-  ALL_NS_LANGS,
-} from '#/common/tolgee/shared'
+import { getStaticData, getLocaleObj } from '#/common/tolgee/shared'
 import { TolgeeNextProvider } from '#/common/tolgee/client'
-import { pathnames } from '#/components/Navigation'
 
 type Props = {
   children: React.ReactNode
@@ -25,60 +17,6 @@ const Layout = async ({
   params: { locale },
   resolvedUrl,
 }: Props) => {
-  // const locales = await getStaticData([locale])
-
-  let localeObj = getLocaleObj(locale)
-  const localeExists = Object.keys(localeObj).length > 0
-  const splitUrl = resolvedUrl.split('/').filter((path) => path !== '')
-
-  if (!localeExists || splitUrl.length > 1) {
-    let matchedPath = null
-
-    // the index of the part of the url that is used for pattern matching
-    // i.e., the part after locale. Or if locale is not found, the first part
-    let urlIndex = 0
-    if (localeExists && splitUrl.length > 1) {
-      urlIndex = 1
-    }
-
-    matchedPath = Object.values(pathnames).find((pathname) => {
-      const splitPathname = pathname.split('/').filter((path) => path !== '')
-      if (splitPathname.length > 0) {
-        return (
-          splitPathname[0].toLowerCase() === splitUrl[urlIndex].toLowerCase()
-        )
-      }
-    })
-
-    // if the path is legit
-    if (matchedPath) {
-      if (Object.keys(ALL_NS_LANGS).includes(matchedPath)) {
-        let appletLocale = locale
-        if (!localeExists) {
-          appletLocale = DEFAULT_LOCALE
-        }
-        const appletPath = matchedPath as keyof typeof ALL_NS_LANGS
-        const localesForNs = getLocalesForNs(appletPath)
-        if (localesForNs.includes(locale)) {
-          if (!localeExists) {
-            // redirect to the global default locale
-            redirect(`/${appletLocale}/${resolvedUrl}`)
-          }
-        } else {
-          appletLocale = localesForNs[0]
-
-          // redirect to default (first) locale for the applet
-          redirect(`/${appletLocale}/${resolvedUrl}`)
-        }
-      }
-    }
-
-    if (!localeExists) {
-      // redirect to the global default locale
-      redirect(`/${DEFAULT_LOCALE}/${resolvedUrl}`)
-    }
-  }
-
   const locales = await getStaticData(getLocaleObj(locale))
 
   return (
---
+++ src/middleware.ts
@@ -0,0 +1,118 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { pathnames } from '#/components/Navigation'
+import {
+  DEFAULT_LOCALE,
+  getLocaleObj,
+  getLocalesForNs,
+  ALL_NS_LANGS,
+} from '#/common/tolgee/shared'
+
+// check out locale validity and redirect if needed
+export function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname
+  let splitUrl: string[] = []
+
+  if (pathname != null && request.nextUrl.pathname !== '' && pathname !== '/') {
+    splitUrl = request.nextUrl.pathname.split('/').filter((path) => path !== '')
+    const locale = splitUrl[0]
+
+    let localeObj = getLocaleObj(locale as string)
+    const localeExists = Object.keys(localeObj).length > 0
+    let localeParamMissing = false
+
+    if (!localeExists || splitUrl.length > 1) {
+      let matchedPath: string | undefined
+
+      // find the possible matching path for the relevant part of the url
+      if (localeExists && splitUrl.length > 1) {
+        matchedPath = findMatchedPath(pathnames, splitUrl[1])
+      } else {
+        matchedPath = findMatchedPath(pathnames, splitUrl[0])
+
+        if (matchedPath != null) {
+          localeParamMissing = true
+        } else if (splitUrl.length > 1) {
+          matchedPath = findMatchedPath(pathnames, splitUrl[1])
+        }
+      }
+
+      // if the path is legit
+      if (matchedPath) {
+        if (Object.keys(ALL_NS_LANGS).includes(matchedPath)) {
+          let appletLocale = locale
+          if (!localeExists) {
+            appletLocale = DEFAULT_LOCALE
+          }
+          const appletPath = matchedPath as keyof typeof ALL_NS_LANGS
+          const localesForNs = getLocalesForNs(appletPath)
+
+          if (localesForNs.includes(locale)) {
+            if (!localeExists) {
+              // redirect to the global default locale
+              if (!localeParamMissing) {
+                // if an invalid locale param was supplied, remove the locale from path
+                return NextResponse.redirect(
+                  new URL(
+                    `/${appletLocale}/${splitUrl.slice(1).join('/')}`,
+                    request.url
+                  )
+                )
+              }
+              return NextResponse.redirect(
+                new URL(`/${appletLocale}${pathname}`, request.url)
+              )
+            }
+          } else {
+            // redirect to default (first) locale for the applet
+            appletLocale = localesForNs[0]
+
+            if (!localeParamMissing) {
+              // if an invalid locale param was supplied, remove the locale from path
+              return NextResponse.redirect(
+                new URL(
+                  `/${appletLocale}/${splitUrl.slice(1).join('/')}`,
+                  request.url
+                )
+              )
+            }
+            return NextResponse.redirect(
+              new URL(`/${appletLocale}/${pathname}`, request.url)
+            )
+          }
+        }
+      }
+
+      if (!localeExists) {
+        // redirect to the global default locale
+        return NextResponse.redirect(new URL(`/${DEFAULT_LOCALE}`, request.url))
+      }
+    }
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: [
+    // match all routes except static files and APIs
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+}
+
+const findMatchedPath = (
+  pathnames: Record<string, string>,
+  urlPath: string
+) => {
+  let match = Object.values(pathnames).find((pathname) => {
+    const splitPathname = pathname.split('/').filter((path) => path !== '')
+    if (splitPathname.length > 0) {
+      return splitPathname[0].toLowerCase() === urlPath.toLowerCase()
+    }
+  })
+
+  if (match != null && match.charAt(0) === '/') {
+    match = match.slice(1)
+  }
+  return match
+}
